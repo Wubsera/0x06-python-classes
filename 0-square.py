27e88bc37baaeb891673b8ec850c70bd4f92c9ab
Wubsera <wubsera21@gmail.com>
2022-10-11 09:47:31 -0700

    0-square.py

--- 0-square.py
+++ 0-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""Square module."""
+
+"""Write an empty class Square that defines a square"""
 
 
 class Square:
-        """Defines a square."""
+        """ Pass Works to do nothing in the class"""
+
         pass
