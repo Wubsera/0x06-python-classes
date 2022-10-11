dfa4ed047fe9343c935fc8f78fe17816f7d06225
Wubsera <wubsera21@gmail.com>
2022-10-11 10:27:57 -0700

    0-square.py

--- 0-square.py
+++ 0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-Square = __import__('0-square').Square
+"""Square module."""
 
-my_square = Square()
-print(type(my_square))
-print(my_square.__dict__)
+
+class Square:
+    """Defines a square."""
+            pass
