1bb3e04083cbba7ef13a06f1965d5bf29d2b11fa
Wubsera <wubsera21@gmail.com>
2022-10-11 10:09:19 -0700

    1-square.py

---
+++ 1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Square module."""
+
+
+class Square:
+   
+    """Defines a square."""
+
+    def __init__(self, size)
+        
+        """Constructor
+
+        args:
+            size: length of a side of the square.
+        """
+        self.__size = size
